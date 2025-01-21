72f66b719f0cb76b42a135addefe3484fa2a6d2b
waka.apps <waka.apps@gmail.com>
2025-01-22 01:41:10 +0900

    ヘッダリンクを.env使用に修正

--- app/components/header/StickyMenu.tsx
+++ app/components/header/StickyMenu.tsx
@@ -11,12 +11,12 @@ export const StickyMenu: FC = () => {
   return (
     <header className={`sticky top-0 w-full pt-8 bg-white`}>
       <div className="flex space-x-4 border-b mr-8 border-gray-200 ">
-        <a href="https://wakaapps.com">
+        <a href={`${import.meta.env.VITE_MAIN_SITE_URL}`}>
           <div className={`border-b w-14 text-center pb-2 ${blogBorder}`}>
             <span className="text-gray-600 text-sm">Blog</span>
           </div>
         </a>
-        <a href="https://wakaapps.com/about">
+        <a href={`${import.meta.env.VITE_MAIN_SITE_URL}/about`}>
           <div className={`border-b w-14 text-center pb-2 ${aboutBorder}`}>
             <span className="text-gray-600 text-sm">About</span>
           </div>
