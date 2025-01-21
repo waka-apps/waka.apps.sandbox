877ad7ffc5315202f15d232f19a14a5fdb30b83b
waka.apps <waka.apps@gmail.com>
2025-01-21 23:51:10 +0900

    StickyMenuの下部ボーダーカラー調整

--- app/components/header/StickyMenu.tsx
+++ app/components/header/StickyMenu.tsx
@@ -4,22 +4,26 @@ import type { FC } from "react";
 export const StickyMenu: FC = () => {
   const location = useLocation();
 
-  const blogBorder =
-    location.pathname === "/" ? "border-gray-600" : "border-gray-200";
-  const aboutBorder =
-    location.pathname === "/about" ? "border-gray-600" : "border-gray-200";
+  const blogBorder = location.pathname === "border-gray-200";
+  const aboutBorder = location.pathname === "border-gray-200";
+  const sandboxBorder = location.pathname === "/" ? "border-gray-600" : "border-gray-200";
 
   return (
     <header className={`sticky top-0 w-full pt-8 bg-white`}>
       <div className="flex space-x-4 border-b mr-8 border-gray-200 ">
-        <Link to={"/"}>
+        <a href="https://wakaapps.com">
           <div className={`border-b w-14 text-center pb-2 ${blogBorder}`}>
             <span className="text-gray-600 text-sm">Blog</span>
           </div>
-        </Link>
-        <Link to={"/about"}>
+        </a>
+        <a href="https://wakaapps.com/about">
           <div className={`border-b w-14 text-center pb-2 ${aboutBorder}`}>
             <span className="text-gray-600 text-sm">About</span>
+          </div>
+        </a>
+        <Link to={"/"}>
+          <div className={`border-b w-14 text-center pb-2 ${sandboxBorder}`}>
+            <span className="text-gray-600 text-sm">Sandbox</span>
           </div>
         </Link>
       </div>
